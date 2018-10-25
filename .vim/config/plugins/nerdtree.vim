dbab3858f53aa524cbb805dfc536a69e6dab0f9c
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2018-10-25 15:05:07 +0900

    remove dein.vim

--- .vim/config/plugins/nerdtree.vim
+++
@@ -1,9 +0,0 @@
-"ファイル指定なしの場合、NERDTreeを表示
-autocmd StdinReadPre * let s:std_in=1
-autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
-
-"不可視ファイルを表示
-let NERDTreeShowHidden = 1
-
-"NERDTreeを表示するショートカット
-map <C-n> :NERDTreeToggle<CR>
--- dotfile-link.sh
+++ dotfile-link.sh
@@ -3,9 +3,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim/config/basic.vim ~/.vim/config/basic.vim
 ln -sf ~/dotfiles/.vim/config/display.vim ~/.vim/config/display.vim
 ln -sf ~/dotfiles/.vim/config/search.vim ~/.vim/config/search.vim
-ln -sf ~/dotfiles/.vim/config/dein.vim ~/.vim/config/dein.vim
-ln -sf ~/dotfiles/.vim/config/plugins/neocomplete.vim ~/.vim/config/plugins/neocomplete.vim
-ln -sf ~/dotfiles/.vim/config/plugins/nerdtree.vim ~/.vim/config/plugins/nerdtree.vim
 #ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
