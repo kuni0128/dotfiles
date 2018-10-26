38d5e1d1475d91af216c5a4c8233e355bfc74a07
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2018-10-26 20:27:54 +0900

    add neocomplete, color-theme plugins

--- setup-vim.sh
+++ setup-vim.sh
@@ -13,10 +13,15 @@ mkdir -p "${vim_config_path}/plugins"
 
 # git clone vim plugins with load at start
 cd $package_start_path
-git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/nerdtree.git                #ツリー表示
+git clone https://github.com/Shougo/neocomplete.vim.git             #コードの自動補完
+git clone https://github.com/Shougo/neosnippet.vim.git              #スニペットの自動補完
+git clone https://github.com/Shougo/neosnippet-snippets.git         #スニペット集
+git clone https://github.com/chriskempson/vim-tomorrow-theme.git    #カラースキーマ
 
 ## create symbolic link for vim plugins
 ln -sf ~/dotfiles/.vim/config/plugins/nerdtree.vim ~/.vim/config/plugins/nerdtree.vim
+ln -sf ~/dotfiles/.vim/config/plugins/neocomplete.vim ~/.vim/config/plugins/neocomplete.vim
 
 ## create symbolic link for vim configuration
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
